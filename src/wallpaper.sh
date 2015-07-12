6e59d2d3235bbd93bf323c697fa9683e18244e59
skunkie <yurka.michurin@gmail.com>
2015-07-12 23:57:51 +0300

    no log option plus check if the first argument is given

--- src/wallpaper.sh
+++ src/wallpaper.sh
@@ -6,17 +6,26 @@
 # crontab -e
 # * */1 * * * <path to this script file> <path to the folder with wallpapers>
 
+if [ -z $1 ]; then
+    echo "$0 <path to the folder with wallpapers>"
+    exit 1
+fi
+
 FILES="$1"
+NOLOG="$2"
+if [ -z "$NOLOG" ]; then
+    NOLOG="--log"
+fi
 LOG=/tmp/wallpaper.log
 
 # Change the wallpaper to a random .jpg file in the specified folder.
 # Exclude the previously selected file.
 previous=$(                                                 \
     gsettings get org.gnome.desktop.background picture-uri  \
-    | sed 's|file:///||g'                                     \
+    | sed 's|file:///||g'                                   \
     | sed 's|\x27||g' )
 next=$(
-    find $FILES -iname '*.jpg' -type f                      \
+    find "$FILES" -iname '*.jpg' -type f                    \
     | grep -v "$previous"                                   \
     | shuf -n1 )
 result=$(                                                   \
@@ -24,13 +33,13 @@ result=$(                                                   \
     GSETTINGS_BACKEND=dconf                                 \
     gsettings set org.gnome.desktop.background picture-uri  \
     "file:///$next" )
-
 # Logging.
-printf '%s %s %s %s %s\n'                                   \
-    "$(date -u +'%F %T')Z"                                  \
-    "$(whoami)"                                             \
-    "$previous"                                             \
-    "$next"                                                 \
-    "$result"                                               \
-    >> "$LOG"
-
+if [ $NOLOG != "--nolog" ]; then
+    printf '%s %s %s %s %s\n'                               \
+        "$(date -u +'%F %T')Z"                              \
+        "$(whoami)"                                         \
+        "$previous"                                         \
+        "$next"                                             \
+        "$result"                                           \
+        >> "$LOG"
+fi
